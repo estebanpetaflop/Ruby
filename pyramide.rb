1247f69a67cbd75d6182071ceae170e98526570a
estebanpetaflop <45972335+estebanpetaflop@users.noreply.github.com>
2019-01-04 19:47:43 +0100

    Update pyramide.rb

--- pyramide.rb
+++ pyramide.rb
@@ -1,7 +1,11 @@
 puts "Yo Keops, cb d'étages à ta pyramide?"
 print "> "
 floor = gets.chomp.to_i
+if floor>25
+  puts "C'est troooop maiiiitre, 25 maximum SVP !"
+else
 puts "Voici maîîîître:"
 for i in 1..floor
   puts " "*(floor-i)+"#"*i
+end
 end
